6db8a6572d0ab973ad554a40524134020ee43e0e
vaibhav9850 <vaibhavshelar3005@gmail.com>
2020-08-21 19:40:06 +0530

    last commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,19 @@
-const env = require('./env.js');
+/*const env = require('./env.js');
  
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
   dialect: env.dialect,
+  operatorsAliases: false,
+ 
+   
+});
+*/
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('dafiqcm6bsgnvp', 'pkuqubdquniyqv','2c4cb9184b8001757d715022186345782032bcc749398bc67e94bc11a158bb50', {
+  host: 'ec2-54-158-122-162.compute-1.amazonaws.com',
+  dialect:'postgres',
   operatorsAliases: false,
  
    
